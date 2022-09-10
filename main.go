151bca3168bbc91ebd9e5e00ba16fbf13b3a8ba0
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-09-10 13:44:11 +0900

    subcommands

--- main.go
+++ main.go
@@ -10,9 +10,48 @@ import (
 
 func main() {
     app := &cli.App{
-        Action: func(cCtx *cli.Context) error {
-            fmt.Printf("Hello %q", cCtx.Args().Get(0))
-            return nil
+        Commands: []*cli.Command{
+            {
+                Name:    "add",
+                Aliases: []string{"a"},
+                Usage:   "add a task to the list",
+                Action: func(cCtx *cli.Context) error {
+                    fmt.Println("added task: ", cCtx.Args().First())
+                    return nil
+                },
+            },
+            {
+                Name:    "complete",
+                Aliases: []string{"c"},
+                Usage:   "complete a task on the list",
+                Action: func(cCtx *cli.Context) error {
+                    fmt.Println("completed task: ", cCtx.Args().First())
+                    return nil
+                },
+            },
+            {
+                Name:    "template",
+                Aliases: []string{"t"},
+                Usage:   "options for task templates",
+                Subcommands: []*cli.Command{
+                    {
+                        Name:  "add",
+                        Usage: "add a new template",
+                        Action: func(cCtx *cli.Context) error {
+                            fmt.Println("new task template: ", cCtx.Args().First())
+                            return nil
+                        },
+                    },
+                    {
+                        Name:  "remove",
+                        Usage: "remove an existing template",
+                        Action: func(cCtx *cli.Context) error {
+                            fmt.Println("removed task template: ", cCtx.Args().First())
+                            return nil
+                        },
+                    },
+                },
+            },
         },
     }
 
